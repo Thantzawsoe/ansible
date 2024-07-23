977b2dd63944e5ebae24cd50fd08789a233ce0d7
Thantzawsoe <54613765+Thantzawsoe@users.noreply.github.com>
2024-07-24 00:41:18 +0630

    Delete install_wazuh_agent.sh

--- install_wazuh_agent.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-wget https://packages.wazuh.com/4.x/apt/pool/main/w/wazuh-agent/wazuh-agent_4.8.0-1_amd64.deb && sudo WAZUH_MANAGER='192.168.30.194' dpkg -i ./wazuh-agent_4.8.0-1_amd64.deb
-
-sudo systemctl daemon-reload
-sudo systemctl enable wazuh-agent
-sudo systemctl start wazuh-agent
