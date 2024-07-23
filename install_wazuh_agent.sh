576f03cfc92150f2f43382db36b4839e61314e69
Thantzawsoe <54613765+Thantzawsoe@users.noreply.github.com>
2024-07-24 00:15:33 +0630

    Update install_wazuh_agent.sh

--- install_wazuh_agent.sh
+++ install_wazuh_agent.sh
@@ -1,38 +1,6 @@
 #!/bin/bash
+wget https://packages.wazuh.com/4.x/apt/pool/main/w/wazuh-agent/wazuh-agent_4.8.0-1_amd64.deb && sudo WAZUH_MANAGER='192.168.30.194' dpkg -i ./wazuh-agent_4.8.0-1_amd64.deb
 
-# Define variables
-WAZUH_MANAGER="192.168.30.194"
-WAZUH_AGENT_VERSION="4.8.0-1"
-WAZUH_AGENT_DEB="wazuh-agent_${WAZUH_AGENT_VERSION}_amd64.deb"
-
-# Function to handle errors
-handle_error() {
-    echo "An error occurred on line $1"
-    exit 1
-}
-
-# Trap errors
-trap 'handle_error $LINENO' ERR
-
-# Download Wazuh agent package
-echo "Downloading Wazuh agent..."
-wget https://packages.wazuh.com/4.x/apt/pool/main/w/wazuh-agent/${WAZUH_AGENT_DEB} -O /tmp/${WAZUH_AGENT_DEB} || handle_error $LINENO
-
-# Install Wazuh agent package
-echo "Installing Wazuh agent..."
-sudo WAZUH_MANAGER="${WAZUH_MANAGER}" dpkg -i /tmp/${WAZUH_AGENT_DEB} || handle_error $LINENO
-
-# Reload systemd manager configuration
-echo "Reloading systemd manager configuration..."
-sudo systemctl daemon-reload || handle_error $LINENO
-
-# Enable and start Wazuh agent service
-echo "Enabling and starting Wazuh agent service..."
-sudo systemctl enable wazuh-agent || handle_error $LINENO
-sudo systemctl start wazuh-agent || handle_error $LINENO
-
-# Clean up
-echo "Cleaning up..."
-rm -f /tmp/${WAZUH_AGENT_DEB} || handle_error $LINENO
-
-echo "Wazuh agent installation and setup complete."
+sudo systemctl daemon-reload
+sudo systemctl enable wazuh-agent
+sudo systemctl start wazuh-agent
