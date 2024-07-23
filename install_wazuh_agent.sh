81cf06c710643114f922da38c6aaae7902720287
Thantzawsoe <54613765+Thantzawsoe@users.noreply.github.com>
2024-07-24 00:07:00 +0630

    Update install_wazuh_agent.sh

--- install_wazuh_agent.sh
+++ install_wazuh_agent.sh
@@ -1,5 +1,24 @@
-wget https://packages.wazuh.com/4.x/apt/pool/main/w/wazuh-agent/wazuh-agent_4.8.0-1_amd64.deb && sudo WAZUH_MANAGER='192.168.30.194' dpkg -i ./wazuh-agent_4.8.0-1_amd64.deb
+#!/bin/bash
 
+# Define variables
+WAZUH_MANAGER="192.168.30.194"
+WAZUH_AGENT_VERSION="4.8.0-1"
+WAZUH_AGENT_DEB="wazuh-agent_${WAZUH_AGENT_VERSION}_amd64.deb"
+
+# Download Wazuh agent package
+wget https://packages.wazuh.com/4.x/apt/pool/main/w/wazuh-agent/${WAZUH_AGENT_DEB}
+
+# Install Wazuh agent package
+sudo WAZUH_MANAGER="${WAZUH_MANAGER}" dpkg -i ./${WAZUH_AGENT_DEB}
+
+# Reload systemd manager configuration
 sudo systemctl daemon-reload
+
+# Enable and start Wazuh agent service
 sudo systemctl enable wazuh-agent
 sudo systemctl start wazuh-agent
+
+# Clean up
+rm -f ./${WAZUH_AGENT_DEB}
+
+echo "Wazuh agent installation and setup complete."
